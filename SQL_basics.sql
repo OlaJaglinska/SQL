b982d561e566bfacbe9dcebff4e0939bdc9b9075
Ola <olga.jaglinska@gmail.com>
2022-07-24 15:38:02 +0200

    SQL_lesson2

--- SQL_basics.sql
+++ SQL_basics.sql
@@ -1,5 +1,5 @@
 SELECT user_name FROM users;
-INSERT INTO users(user_name, display_name, year_of_birth) VALUES('olaj', 'Ola Jaglinska', '1996');
+INSERT INTO users(user_name, display_name, year_of_birth) VALUES('olaj', 'Ola', '1996');
 SELECT user_name, display_name, year_of_birth FROM users;
 INSERT INTO users(user_name, display_name, year_of_birth) VALUES 
 ('adam', 'Adam', 1987),
@@ -19,3 +19,41 @@ SELECT * FROM users WHERE year_of_birth BETWEEN 1987 AND 1993;
 SELECT DISTINCT year_of_birth FROM users 
 WHERE year_of_birth IS NOT NULL
 ORDER BY year_of_birth DESC LIMIT 0, 1;
+
+# lesson 2
+
+SELECT * FROM users;
+SELECT * FROM user_groups;
+SELECT * FROM users_to_groups;
+INSERT INTO user_groups(group_name) VALUES ('programmers'), ('skiers'), ('nature lovers'), ('history experts');
+INSERT INTO users_to_groups(user_id, group_id) VALUES(1,1), (1,2), (2,1), (1,3), (2,4), (3,3),(4,1);
+
+SELECT COUNT(user_name) FROM users;
+SELECT MIN(year_of_birth) FROM users;
+SELECT MAX(year_of_birth) FROM users;
+SELECT SUM(year_of_birth) FROM users;
+SELECT AVG(year_of_birth) FROM users;
+
+# SUBQUERY
+SELECT user_name FROM users WHERE id IN
+(SELECT user_id FROM users_to_groups WHERE group_id = 2);
+
+SELECT usersTable.id AS userId,user_name, group_name FROM users AS usersTable
+JOIN users_to_groups ON usersTable.id = users_to_groups.user_id
+JOIN user_groups ON users_to_groups.group_id = user_groups.id;
+
+SELECT user_groups.group_name, COUNT(users.user_name) AS number_of_users FROM users
+JOIN users_to_groups ON users.id = users_to_groups.user_id
+JOIN user_groups ON users_to_groups.group_id = user_groups.id
+GROUP BY user_groups.group_name 
+HAVING number_of_users > 2
+ORDER BY number_of_users;
+
+# updating and deleting
+UPDATE users SET user_name='jula123', display_name='Julia 2' WHERE id=3;
+DELETE FROM users WHERE id=3;
+
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM users;
+TRUNCATE user_groups;
+DROP TABLE users;
